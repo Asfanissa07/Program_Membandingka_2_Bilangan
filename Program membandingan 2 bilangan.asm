246090a6a385691219e32f04069e2ff36c115335
Asfanissa07 <121386693+Asfanissa07@users.noreply.github.com>
2023-01-08 10:32:01 +0700

    Update Program membandingan 2 bilangan.asm

--- Program membandingan 2 bilangan.asm
+++ Program membandingan 2 bilangan.asm
@@ -1,4 +1,3 @@
-
 .MODEL SMALL
 .CODE
 ORG 100h
@@ -7,14 +6,13 @@ DATA:
     JMP Output	
 	Judul DB 10,13,10,13,'=== Program Membandingkan 2 Bilangan Bulat === $'  
 	Garis DB 10,13,'============================================== $'
-	bil1  DB 10,13,'Masukkan angka pertama  : $'
-	bil2  DB 10,13,'Masukkan angka kedua    : $' 
-	Kal1 DB 10,13,'Bilangan ke-1 lebih kecil dr bilangan ke-2 $'
-	Kal2 DB 10,13,'Bilangan ke-1 lebih besar dr bilangan ke-2 $'
-	Kal3 DB 10,13,'Bilangan ke-1 besarnya sama dgn bilangan ke-2 $'
-	Ty   DB 10,13,10,13,'Terimakasih telah menggunakan aplikasi ini ^_^ $'
-	
-	
+	bil1  DB 10,13,'Masukkan bilangan pertama  : $'
+	bil2  DB 10,13,'Masukkan bilangan kedua    : $' 
+	Kal1  DB 10,13,'Bilangan ke-1 lebih kecil dari bilangan ke-2 $'
+	Kal2  DB 10,13,'Bilangan ke-1 lebih besar dari bilangan ke-2 $'
+	Kal3  DB 10,13,'Bilangan ke-1 besarnya sama dgn bilangan ke-2 $'
+	Ty    DB 10,13,10,13,'Terimakasih telah menggunakan aplikasi ini ^_^ $'
+	  
 Output:
     
     MOV DX, OFFSET Judul
@@ -46,21 +44,19 @@ Input:
     
     CMP CL, DL
     JE Sama
-    JL Kecil
+    JB Kecil
     
     MOV DX, OFFSET Kal2   ;Bilangan ke-1 lebih besar dr bilangan ke-2
     MOV AH, 09h
     INT 21h   
 	JMP Exit   
            
-    
 Kecil: 
     MOV DX, OFFSET Kal1   ;Bilangan ke-1 lebih kecil dr bilangan ke-2
     MOV AH, 09h
     INT 21h
     JMP Exit  
-	  
-	
+	  	
 Sama:       
     MOV DX, OFFSET Kal3   ;Bilangan ke-1 besarnya sama dgn bilangan ke-2
     MOV AH, 09h
@@ -72,3 +68,4 @@ Exit:
     MOV AH, 09h 
     INT 21h
 END DATA
+
